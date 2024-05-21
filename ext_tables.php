56f239a6a7deb90bfe04c7c0d0956b369b52d98b
Clemens Gogoljn <service@cylancer.net>
2024-05-21 22:53:08 +0200

    Merge remote-tracking branch 'origin/development' into development

--- ext_tables.php
+++ ext_tables.php
@@ -1,3 +1,11 @@
 <?php
 
+<<<<<<< HEAD
 defined('TYPO3') || die('Access denied.');
+=======
+<<<<<<< HEAD
+defined('TYPO3') || die('Access denied.');
+=======
+defined('TYPO3_MODE') || die('Access denied.');
+>>>>>>> main
+>>>>>>> origin/development
